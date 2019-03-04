f2948483a4fda4da79d7a5e61b247569641605eb
nicocuprinsu <38690296+nicocuprinsu@users.noreply.github.com>
2019-03-04 16:02:41 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -1,3 +1,3 @@
 CREATE INDEX ind_srid ON CourseRegistrations(StudentRegistrationId);
-CREATE MATERIALIZED VIEW Info AS (SELECT cr.StudentRegistrationId, SUM(cr.Grade * c.ECTS)/(SUM(ECTS)*1.0) AS GPA, SUM(ECTS) AS ObtainedECTS FROM CourseRegistrations cr, CourseOffers co, Courses c WHERE cr.CourseOfferId = co.CourseOfferId AND co.courseId = c.courseId GROUP BY cr.StudentRegistrationId);
+CREATE MATERIALIZED VIEW Info AS (SELECT cr.StudentRegistrationId, SUM(cr.Grade * c.ECTS)/(SUM(ECTS)*1.0) AS GPA, SUM(ECTS) AS ObtainedECTS FROM CourseRegistrations cr, CourseOffers co, Courses c WHERE cr.CourseOfferId = co.CourseOfferId AND co.courseId = c.courseId AND cr.Grade >= 5 GROUP BY cr.StudentRegistrationId);
 CREATE MATERIALIZED VIEW Calc AS (SELECT CourseId, Grade, COUNT(*) AS nr FROM CourseOffers co, CourseRegistrations cr WHERE co.CourseOfferId = cr.CourseOfferID AND cr.Grade is not null GROUP BY CourseId, grade); 
