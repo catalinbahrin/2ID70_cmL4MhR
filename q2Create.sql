1547bedbc79d164c1051aeab8821f122a3350677
nicocuprinsu <38690296+nicocuprinsu@users.noreply.github.com>
2019-03-04 04:46:59 +0100

    Update q2Create.sql
    Info is now split in two other views

--- q2Create.sql
+++ q2Create.sql
@@ -1,2 +1,3 @@
 CREATE INDEX ind_srid ON CourseRegistrations(StudentRegistrationId);
-CREATE MATERIALIZED VIEW Info(StudentRegistrationId, GPA, ObtainedECTS, Failed) AS (SELECT cr.StudentRegistrationId, SUM(case when Grade >= 5 then cr.Grade * c.ECTS end)/SUM(case when Grade >= 5 then ECTS*1.0 end), SUM(case when Grade >= 5 then ECTS end), MIN(Grade) FROM CourseRegistrations cr, CourseOffers co, Courses c WHERE cr.CourseOfferId = co.CourseOfferId AND co.courseId = c.courseId GROUP BY cr.StudentRegistrationId);
+CREATE MATERIALIZED VIEW Info AS SELECT cr.StudentRegistrationId, SUM(cr.Grade * c.ECTS)/(SUM(ECTS)*1.0) AS GPA, SUM(ECTS) AS ObtainedECTS FROM CourseRegistrations cr, CourseOffers co, Courses c WHERE cr.CourseOfferId = co.CourseOfferId AND co.courseId = c.courseId AND cr.Grade >= 5 GROUP BY cr.StudentRegistrationId);
+CREATE MATERIALIZED VIEW MinGrade AS (SELECT cr.StudentRegistrationId, Min(Grade) mini FROM CourseRegistrations cr GROUP BY cr.StudentRegistrationId);
