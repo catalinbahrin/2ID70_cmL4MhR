d621acfce8f7ecd660e0b01df84ebb1f2692b160
nicocuprinsu <38690296+nicocuprinsu@users.noreply.github.com>
2019-03-04 05:35:36 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -1,3 +1,2 @@
 CREATE INDEX ind_srid ON CourseRegistrations(StudentRegistrationId);
 CREATE MATERIALIZED VIEW Info AS (SELECT cr.StudentRegistrationId, SUM(cr.Grade * c.ECTS)/(SUM(ECTS)*1.0) AS GPA, SUM(ECTS) AS ObtainedECTS FROM CourseRegistrations cr, CourseOffers co, Courses c WHERE cr.CourseOfferId = co.CourseOfferId AND co.courseId = c.courseId AND cr.Grade >= 5 GROUP BY cr.StudentRegistrationId);
-CREATE MATERIALIZED VIEW MinGrade AS (SELECT cr.StudentRegistrationId, Min(Grade) mini FROM CourseRegistrations cr GROUP BY cr.StudentRegistrationId);
